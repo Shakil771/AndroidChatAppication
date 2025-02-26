a1eac4d9ab4902ea6fb0806e36d207f7d873e65d
Shakil771 <shakilhossaint58@gmail.com>
2025-02-27 01:04:00 +0600

    Updated

--- Server/src/config/firebase.js
+++ Server/src/config/firebase.js
@@ -1,10 +1,25 @@
 const admin = require("firebase-admin");
+import dotenv from "dotenv";
+dotenv.config();
 
-const serviceAccount = require("/serviceAccountKey.json"); // Firebase Service Account Key
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: privateKey,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://your-project-id.firebaseio.com", // Realtime Database URL
+  databaseURL: "https://chat-up-6441e-default-rtdb.firebaseio.com/", // Realtime Database URL
 });
 
 const firestore = admin.firestore();
